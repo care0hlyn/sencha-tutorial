c127d2729bf9034f6511072df076f35f5fbc2ef5
Anna Valkov & Carolyn Huynh <student@epicodus.com>
2014-09-24 11:10:46 -0700

    Sencha stuff

---
+++ build/temp/development/MyApp/sass/config.rb
@@ -0,0 +1,3 @@
+require '../../../../../ext/packages/ext-theme-base/sass/utils.rb'
+require '../../../../../sass/config.rb'
+cache_path = '/Users/epicodus/Code/app/build/.sass-cache'
---
+++ build/temp/development/MyApp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /Users/epicodus/Code/app/app.js
